a00edb1a932071e0d5f902cc17a2c423dbea4141
HRubyta <200183528@student.georgianc.on.ca>
2015-07-14 09:12:55 +0700

    adding sorting function

--- comp2084-lesson9/students.aspx.cs
+++ comp2084-lesson9/students.aspx.cs
@@ -67,10 +67,46 @@ namespace comp2084_lesson9
 
         protected void grdStudents_Sorting(object sender, GridViewSortEventArgs e)
         {
-            //grdStudents.SortDirection = e.SortDirection;
-            GetStudents();
-
+            //Retrieve table from the session object
+            DataTable dt = Session["Student"] as DataTable;
+            
+            if (dt != null)
+            {
+                //sort student data
+                dt.DefaultView.Sort = e.SortExpression + " " + GetSortDirection(e.SortExpression);
+                grdStudents.DataSource = Session["Student"];
+                grdStudents.DataBind();
+            }
         }
-
+        
+        private string GetSortDirection(string column)
+        {
+            //by default,set sort direction to ascending 
+            string sortDirection = "ASC";
+            
+            //retrieve the last column that was sorted
+            string sortExpression = ViewState["SortExpression"] as string;
+            
+            if (sortExpression != null)
+            {
+                //check if the same column is being sorted
+                //otherwise default value can be return
+                if (sortExpression == column)
+                {
+                    string lastDirection = ViewState["SortDirection"] as string;
+                    if((lastDirection != null) && (lastDirection == "ASC"))
+                    {
+                        sortDirection = "DESC";
+                    }
+                }
+            
+            }
+            
+            //Save new values in ViewState
+            ViewState["SortDirection"] = sortDirection;
+            ViewState["SortExpression"] = column;
+        
+            return sortDirection;
+        }
     }
 }
