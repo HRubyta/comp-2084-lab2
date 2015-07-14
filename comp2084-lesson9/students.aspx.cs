d3df83e8647ec475fd1fe2ab39062df333971dd6
HRubyta <200183528@student.georgianc.on.ca>
2015-07-14 07:34:29 +0700

    adding classes to enable sorting features

--- comp2084-lesson9/students.aspx.cs
+++ comp2084-lesson9/students.aspx.cs
@@ -1,9 +1,11 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
 
 //add a reference in use of EF for the database
 using comp2084_lesson9.Models;
